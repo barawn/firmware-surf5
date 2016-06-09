0ba66d103df152ccbfdc535f100f0e0848f4631b
Patrick Allison <allison.122@osu.edu>
2016-06-09 13:47:55 -0400

    damnit reset level was backwards and we was unconnected

--- rtl/rfp_top.v
+++ rtl/rfp_top.v
@@ -21,8 +21,9 @@ module rfp_top(
 	assign sda_in = RFP_SDA;
 	assign RFP_SCL = (scl_oen) ? 1'bZ : scl_out;
 	assign RFP_SDA = (sda_oen) ? 1'bZ : sda_out;
-	i2c_master_top u_top(.wb_clk_i(clk_i),.wb_rst_i(rst_i),.arst_i(1'b1),.wb_adr_i(wb_adr_i[2:0]),.wb_dat_i(wb_dat_i[7:0]),.wb_dat_o(i2c_dat),
+	i2c_master_top u_top(.wb_clk_i(clk_i),.wb_rst_i(rst_i),.arst_i(1'b0),.wb_adr_i(wb_adr_i[2:0]),.wb_dat_i(wb_dat_i[7:0]),.wb_dat_o(i2c_dat),
 								.wb_cyc_i(wb_cyc_i),.wb_stb_i(wb_adr_i[15] == 0 && wb_stb_i),.wb_ack_o(i2c_ack),.wb_err_o(i2c_err),.wb_rty_o(i2c_rty),
+								.wb_we_i(wb_we_i_),
 								.wb_sel_i(wb_stb_i),
 								.scl_pad_i(scl_in),.scl_pad_o(scl_out),.scl_padoen_o(scl_oen),
 								.sda_pad_i(sda_in),.sda_pad_o(sda_out),.sda_padoen_o(sda_oen));
